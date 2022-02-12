c73da70a2dbd3e3c28558b9b6af89194095c9016
Jaygeepd <jgmahon13@gmail.com>
2022-02-12 22:19:12 +0000

    Created empty pages, navbar component and footer component along with some basic styling. Routing setup to navigate to the empty pages from the navbar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navigation from './Components/Navigation';
+import About from './Pages/About';
+import Music from './Pages/Music';
+import Gallery from './Pages/Gallery';
+import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+import Tour from './Pages/Tour';
+import Footer from './Components/Footer'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" exact element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/music" element={<Music />}/>
+          <Route path="/gallery" element={<Gallery />}/>
+          <Route path="/shop" element={<Shop />}/>
+          <Route path="/tour" element={<Tour />}/>
+        </Routes>
+        <Footer />
+      </div> 
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap"; 
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import '../App.css';
+
+function Navigation(){
+    return (
+        <Navbar className="no-horizontal-padding" sticky="top" expand='md'>
+            <Container>
+                <LinkContainer to='/'>
+                    <Navbar.Brand className='title-text' href="#home">Van<span className="subtext">The</span>Man</Navbar.Brand>
+                </LinkContainer>
+                
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav>
+                        <LinkContainer to='/about'>
+                            <Nav.Link className='page-link-text'>About</Nav.Link>
+                        </LinkContainer>
+                        
+                        <LinkContainer to='/music'>
+                            <Nav.Link className='page-link-text'>Music</Nav.Link>
+                        </LinkContainer>
+                        
+                        <LinkContainer to='/gallery'>
+                            <Nav.Link className='page-link-text'>Photos</Nav.Link>
+                        </LinkContainer>
+                        
+                        <LinkContainer to='/shop'>
+                            <Nav.Link className='page-link-text'>Shop</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to='/tour'>
+                            <Nav.Link className='page-link-text'>Tour</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        );
+}
+
+export default Navigation;
