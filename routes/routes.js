ed64e99fa1ccab643424673ad98fb0dbfdcba7d5
Ben McKerry <47438934+BenMcKerry@users.noreply.github.com>
2022-03-17 13:15:47 -0400

    added backend validation for posts/replies

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const GuestbookPostingModel = require('../models/Message');
+const validator = require('validator');
 
 // Routes prefixed with /api
 router.post('/insert', async (req, res) => {
     const { message, name, email } = req.body.post;
-    // console.log(req.body.post)
+    if (!validator.isEmail(email) || validator.isEmpty(name) || validator.isEmpty(message)) {
+        res.status(422).json({ msg: 'Invalid input submitted, please try again' })
+    }
     const posting = new GuestbookPostingModel({ message: message, name: name, email: email, dateTime: new Date() });
     console.log(posting)
     try {
@@ -19,6 +22,9 @@ router.post('/insert', async (req, res) => {
 router.post('/addReply', async (req, res) => {
     const postId = req.body.postId;
     const { message, name, email } = req.body.reply;
+    if (!validator.isEmail(email) || validator.isEmpty(name) || validator.isEmpty(message)) {
+        res.status(422).json({ msg: 'Invalid input submitted, please try again' })
+    }
     GuestbookPostingModel.findById(postId).then((posting) => {
         posting.replies.push({
             name: name,
