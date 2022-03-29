546d548daab3294a94c3edc8f5c69ebc0a4c0413
John Mahon <49533535+Jaygeepd@users.noreply.github.com>
2022-03-29 10:56:06 +0100

    Gallery responsiveness improved - Note slight addition needed for the navbar. When home logo is selected the previous page that was selected is not de-selected

--- client/src/Pages/Gallery.js
+++ client/src/Pages/Gallery.js
@@ -19,56 +19,62 @@ function Gallery(){
         <>
             <Container>
                 <Row className="image-row">
-                    <Col xs={2} />
-                    <Col xs={2}>
-                        <img src={ galImg1 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={3}>
+                        <img src={ galImg1 } alt="image_1" />
                     </Col>
-                    <Col xs={4}>
-                        <img src={ galImg2 } />
+                    <Col xs={12} md={4}>
+                        <img src={ galImg2 } alt="image_2" />
                     </Col>
-                    <Col xs={2}>
-                        <img src={ galImg3 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={2}>
+                        <img src={ galImg3 } alt="image_3" />
                     </Col>
                 
                 </Row>
                 <Row className="image-row">
-                    <Col xs={2} />
-                    <Col xs={2}>
-                        <img src={ galImg4 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={3}>
+                        <img src={ galImg4 } alt="image_4" />
                     </Col>
-                    <Col xs={4}>
-                        <img src={ galImg5 } />
+                    <Col xs={12} md={4}>
+                        <img src={ galImg5 } alt="image_5" />
                     </Col>
-                    <Col xs={2}>
-                        <img src={ galImg6 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={2}>
+                        <img src={ galImg6 } alt="image_6" />
                     </Col>
                 </Row>
                 <Row className="image-row">
-                    <Col xs={2} />
-                    <Col>
-                        <img src={ galImg7 } />
+                    <Col xs={3} md={1}/>
+                    <Col xs={9} md={2}>
+                        <img src={ galImg7 } alt="image_7" />
                     </Col>
-                    <Col>
-                        <img src={ galImg8 } />
+                    <Col xs={3} md={1}/>
+                    <Col xs={9} md={2}>
+                        <img src={ galImg8 } alt="image_8" />
                     </Col>
-                    <Col>
-                        <img src={ galImg9 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={2}>
+                        <img src={ galImg9 } alt="image_9" />
                     </Col>
-                    <Col>
-                        <img src={ galImg10 } />
+                    <Col xs={3} md={1} />
+                    <Col xs={9} md={2}>
+                        <img src={ galImg10 } alt="image_10" />
                     </Col>
                     <Col xs={2} />
                 </Row>
                 <Row className="image-row">
-                    <Col xs={2} />
-                    <Col xs={2}>
-                        <img src={ galImg11 } />
+                    <Col xs={2} md={1} />
+                    <Col xs={10} md={4}>
+                        <img src={ galImg11 } alt="image_11" />
                     </Col>
-                    <Col xs={4}>
-                        <img src={ galImg12 } />
+                    <Col xs={12} md={3}>
+                        <img src={ galImg12 } alt="image_12" />
                     </Col>
-                    <Col xs={2}>
-                        <img src={ galImg13 } />
+                    <Col xs={3} md={2} />
+                    <Col xs={6} md={1}>
+                        <img src={ galImg13 } alt="image_13" />
                     </Col>
                 </Row>
                 <Row className="separate-row">
